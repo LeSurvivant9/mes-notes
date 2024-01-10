43dc43d0990b7da5429b787a48d2a5bc7d06ebcf
LeSurvivant_ <mcalexandreboisfer@gmail.com>
2024-01-10 13:27:38 +0100

    Project initialized

---
+++ prisma/migrations/20240110010556_initial_migration/migration.sql
@@ -0,0 +1,122 @@
+-- CreateTable
+CREATE TABLE `department` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `department_name` VARCHAR(50) NOT NULL,
+
+    UNIQUE INDEX `department_department_name_key`(`department_name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `teaching_unit` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `teaching_unit_name` VARCHAR(50) NOT NULL,
+    `department_id` INTEGER NOT NULL,
+
+    INDEX `teaching_unit_index_0`(`department_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subject` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `teaching_unit_id` INTEGER NOT NULL,
+    `subject_name` VARCHAR(50) NOT NULL,
+    `subject_coefficient` INTEGER NOT NULL,
+
+    INDEX `subject_index_1`(`teaching_unit_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `student_id` INTEGER NULL,
+    `professor_id` INTEGER NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `first_name` VARCHAR(50) NOT NULL,
+    `username` VARCHAR(30) NOT NULL,
+    `image` VARCHAR(255) NULL,
+    `hashed_password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `email_verified` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `user_username_key`(`username`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `professor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `student` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `student_number` VARCHAR(8) NOT NULL,
+    `department_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `entry_year` INTEGER NULL,
+    `level` INTEGER NOT NULL,
+
+    UNIQUE INDEX `student_student_number_key`(`student_number`),
+    INDEX `student_index_2`(`student_number`),
+    INDEX `student_index_3`(`department_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `assessment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `subject_id` INTEGER NOT NULL,
+    `type_of_assessment` VARCHAR(50) NOT NULL,
+    `date_of_assessment` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `coefficient` INTEGER NOT NULL,
+    `file_name` VARCHAR(255) NOT NULL,
+    `period` INTEGER NOT NULL,
+    `semester` INTEGER NOT NULL,
+
+    INDEX `assessment_index_4`(`subject_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `grade` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `student_id` INTEGER NOT NULL,
+    `assessment_id` INTEGER NOT NULL,
+    `grade_value` DOUBLE NOT NULL,
+
+    INDEX `grade_index_5`(`student_id`),
+    INDEX `grade_index_6`(`assessment_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `teaching_unit` ADD CONSTRAINT `teaching_unit_department_id_fkey` FOREIGN KEY (`department_id`) REFERENCES `department`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subject` ADD CONSTRAINT `subject_teaching_unit_id_fkey` FOREIGN KEY (`teaching_unit_id`) REFERENCES `teaching_unit`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user` ADD CONSTRAINT `user_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `student`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user` ADD CONSTRAINT `user_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `professor`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `student` ADD CONSTRAINT `student_department_id_fkey` FOREIGN KEY (`department_id`) REFERENCES `department`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `assessment` ADD CONSTRAINT `assessment_subject_id_fkey` FOREIGN KEY (`subject_id`) REFERENCES `subject`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `grade` ADD CONSTRAINT `grade_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `grade` ADD CONSTRAINT `grade_assessment_id_fkey` FOREIGN KEY (`assessment_id`) REFERENCES `assessment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
