a94bc7ef123dace48416b95c31ae81b5b93675e4
LeSurvivant9 <mcalexandreboisfer@gmail.com>
2024-01-12 11:31:31 +0100

    Deactivate showSocial

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -16,8 +16,8 @@ export const Navbar = () => {
         <nav className={"absolute top-2 bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-md"}>
             <div className={"flex gap-x-2"}>
                 <Button variant={pathname === "/client" ? "default" : "outline"} asChild>
-                    <Link href={"/client"}>
-                        <VscGraph/>Client
+                    <Link href={"/grades"}>
+                        <VscGraph/>Notes
                     </Link>
                 </Button>
                 <Button variant={pathname === "/server" ? "default" : "outline"} asChild>
