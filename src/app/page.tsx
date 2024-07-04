f7655bef7eec3aa5d1fa74b6002d3393b95349c1
LeSurvivant9 <mcalexandreboisfer@gmail.com>
2024-07-04 03:23:30 +0200

    Added search to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,53 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "@/components/navbar";
 import Container from "@/components/ui/container";
 import { ThemeProvider } from "@/theme/ThemeProvider";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GradesComponent from "@/components/student-grades";
 
 export default function Home() {
+  const queryClient = new QueryClient();
+  const [studentNumber, setStudentNumber] = useState("");
+  const [submittedStudentNumber, setSubmittedStudentNumber] = useState<
+    string | null
+  >(null);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStudentNumber(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmittedStudentNumber(studentNumber);
+  };
+
   return (
     <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
-      <Navbar />
-      <Container>
-        <main className={"pt-17"}>
-          <h1 className={"pt-4 text-center"}>
-            Bonjour à toi INSAïen INSAïenne :)
-          </h1>
-          <br />
-          <p>
-            Ce site à pour seul but de regrouper toutes tes notes dans un seul
-            et même endroit, il te permet aussi de calculer automatiquement tes
-            moyennes grâce aux différents coefficients du syllabus.
-          </p>
-          <br />
-          <p>
-            Ce site est toujours en construction, je taffe dessus depuis déjà 2
-            mois pour vous sortir quelque chose de qualitatif, aujourd'hui c'est
-            la release v1.1, et j'ai déjà prévu une v5.0, le site est et sera en
-            constante évolution. De plus, si tu as des idées d'améliorations,
-            n'hésites surtout pas à venir me voir en pv Insta, je suis ouvert à
-            la discussion.
-          </p>
-          <br />
-          <p>
-            L'ajout des notes n'est pas encore automatisé, donc si tu vois qu'il
-            manque certaines notes, n'hésites pas à m'en faire part.
-          </p>
-        </main>
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Container>
+          <main className={"pt-17"}>
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <Input
+                type="text"
+                placeholder="Entrez votre numéro étudiant"
+                value={studentNumber}
+                onChange={handleInputChange}
+                className={"max-w-xs text-center mt-4"}
+              />
+              <Button className={"max-w-xs"} onClick={handleSubmit}>
+                Soumettre
+              </Button>
+            </div>
+            {submittedStudentNumber && (
+              <GradesComponent studentNumber={submittedStudentNumber} />
+            )}
+          </main>
+        </Container>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
